55c7a8e3317ea4d38237a5c1f4cad407c0587f84
LearnerAnuprash <info.anuprash@gmail.com>
2025-05-29 22:38:40 +0545

    Added javascript to code : 1) Alternating between X & O feature added , 2) Different styling for player X and player O made 3) working on checking Winners code

--- TicTacscript.js
+++ TicTacscript.js
@@ -1,3 +1,38 @@
+let boxes=document.querySelectorAll(".clickme");
+let resetBtn=document.querySelector(".res");
 
+let turnO=true; //player O if true
 
+const winningCondition=[
+    [0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]
+];//2D Array
 
+boxes.forEach(
+    function(clickme){
+        clickme.addEventListener("click",()=>{
+            console.log("Box was clicked");
+            if(clickme.textContent==="")
+            {
+            if(turnO)
+            {
+                clickme.textContent="O";
+                clickme.style.color="rgb(199, 3, 78)";
+                turnO=false;
+                
+            }
+
+            else{
+                clickme.textContent="X";
+                clickme.style.color="rgb(82, 7, 255)";
+                turnO=true;
+            }
+            checkWinner();
+
+            }
+        })
+    }
+);
+const checkWinner =()=>
+{
+    
+}
